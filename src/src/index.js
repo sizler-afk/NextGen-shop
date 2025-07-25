eb56a918adaba123919869e30c68d0ca93e2b647
sizler-afk <sahalkhan0901@gmail.com>
2025-07-25 10:07:47 +0530

    Create index.js

---
+++ src/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import FuturisticShop from './App';
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <FuturisticShop />
+  </React.StrictMode>
+);
